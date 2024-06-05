4b5ebfba2f86012d66af4f2b95a08dc857b038e5
rlawlsgud980 <rlawlsgud980@naver.com>
2024-06-05 14:47:21 +0900

    오류 수정

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -2,30 +2,32 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function ProjectItem({ data }) {
-  const title = data.properties.이름.title.plain_text;
-  const github = data.properties.Git.url;
-  const description = data.properties.설명.rich_text[0].plain_text;
-  const imgSrc = data.cover.file?.url || data.cover.external.url;
+  const title = data.properties.이름.title[0]?.plain_text;
+  const github = data.properties.Git?.url;
+  const description = data.properties.설명.rich_text[0]?.plain_text;
+  const imgSrc = data.cover?.file?.url || data.cover?.external.url;
   const tags = data.properties.태그.multi_select;
-  const start = data.properties.날짜.date.start;
-  const end = data.properties.날짜.date.end;
-  const id = data.id; // 이 부분을 Notion 페이지의 ID로 수정
-  const url = id.replace(/-/g, ''); // '-'를 제거하여 URL에 사용
+  const start = data.properties.날짜?.date?.start;
+  const end = data.properties.날짜?.date?.end;
+  const id = data.id;
+  const url = id.replace(/-/g, '');
 
   console.log(url);
 
   const calculatedPeriod = (start, end) => {
+    if (!start || !end) return 0;
+
     const startDateStringArray = start.split('-');
     const endDateStringArray = end.split('-');
 
-    var startDate = new Date(
+    const startDate = new Date(
       startDateStringArray[0],
-      startDateStringArray[1],
+      startDateStringArray[1] - 1,
       startDateStringArray[2]
     );
-    var endDate = new Date(
+    const endDate = new Date(
       endDateStringArray[0],
-      endDateStringArray[1],
+      endDateStringArray[1] - 1,
       endDateStringArray[2]
     );
 
@@ -48,24 +50,25 @@ export default function ProjectItem({ data }) {
         legacyBehavior
       >
         <a>
-          <Image
-            className="rounded-t-xl"
-            src={imgSrc}
-            alt="cover image"
-            width="100%"
-            height="50%"
-            layout="responsive"
-            objectFit="cover"
-            quality={100}
-          />
+          {imgSrc && (
+            <Image
+              className="rounded-t-xl"
+              src={imgSrc}
+              alt="cover image"
+              width={800}
+              height={400}
+              layout="responsive"
+              objectFit="cover"
+              quality={100}
+            />
+          )}
         </a>
       </Link>
 
-      {/* className="m-5 bg-text-green-500" */}
       <div className="p-4 flex flex-col">
         <h1 className="text-2xl font-bold">{title}</h1>
         <h3 className="mt-4 text-xl">{description}</h3>
-        <a href={github}>GitHub 바로가기</a>
+        {github && <a href={github}>GitHub 바로가기</a>}
         <p className="my-1">
           프로젝트 소요 기간 : {start} ~ {end} ({calculatedPeriod(start, end)}
           일)
